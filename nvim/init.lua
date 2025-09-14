03f6311954bac2845ef72584edbe90d7de75b609
David Lopes <david.lopes@dynatrace.com>
2025-09-14 09:09:22 -0500

    autocomplete

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,9 +18,6 @@ vim.pack.add {
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
 
-	-- picker
-	{ src = "https://github.com/nvim-mini/mini.pick" },
-
 	-- telescope
 	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
@@ -28,18 +25,23 @@ vim.pack.add {
 
 	-- theme
 	{ src = "https://github.com/folke/tokyonight.nvim" },
-	{ src = "https://github.com/stevearc/oil.nvim" }
+	{ src = "https://github.com/stevearc/oil.nvim" },
+
+	-- Niceties
+	{ src = "https://github.com/nvim-mini/mini.nvim" }
 }
 
 require("mason").setup()
 require("mason-lspconfig").setup {
-	ensure_installed = { "lua_ls", "ruff", "ts_ls", "eslint", "gopls" },
+	ensure_installed = { "lua_ls", "ruff", "ts_ls", "eslint", "gopls", "pyright" },
 }
 
-require("mini.pick").setup()
 require("telescope").setup()
 require("telescope").load_extension "file_browser"
 require("oil").setup()
+require("mini.pick").setup()
+require("mini.comment").setup()
+require("mini.completion").setup()
 
 vim.cmd [[colorscheme tokyonight-night]]
 
@@ -69,3 +71,24 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 	command = 'LspEslintFixAll',
 	group = vim.api.nvim_create_augroup('EslintAutoFormat', {})
 })
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+	pattern = { '*.py' },
+	callback = function()
+		vim.lsp.buf.code_action {
+			context = {
+				only = { 'source.fixAll' },
+				diagnostics = {}
+			},
+			apply = true,
+		}
+		vim.lsp.buf.format { async = true }
+	end,
+	group = vim.api.nvim_create_augroup('RuffAutoFormat', {})
+})
+
+
+vim.keymap.set('n', 'gK', function()
+	local new_config = not vim.diagnostic.config().virtual_lines
+	vim.diagnostic.config({ virtual_lines = new_config })
+end, { desc = 'Toggle diagnostic virtual_lines' })
